f4afc5ffe3534ca17dbab7c0467d2ba2a6461b5e
Fernweh <780039590@qq.com>
2025-04-27 09:53:05 +0800

    add annotation

--- src/control/launch/control.launch.py
+++ src/control/launch/control.launch.py
@@ -25,6 +25,7 @@ def generate_launch_description():
         'max_angular_velocity':10.0,
         'acceleration_limit':1.0,
         'deceleration_limit':-1.0,
+        'target_spd':2, # 期望速度 m/s
         'adc_traj_topic_name':'/planning/trajectory',
         'localization_info_topic_name':'/localization_info',
         'control_cmd_topic_name':'control/control_cmd'
