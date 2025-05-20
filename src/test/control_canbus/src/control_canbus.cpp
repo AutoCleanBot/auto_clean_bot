9a494812d074a7f22665e84ad905ad1cc9727d2c
Fernweh <780039590@qq.com>
2025-05-20 10:34:03 +0800

    more test

--- src/test/control_canbus/src/control_canbus.cpp
+++ src/test/control_canbus/src/control_canbus.cpp
@@ -25,11 +25,12 @@ class ControlCanbus : public rclcpp::Node {
                 spd = 4.0;
             }
         }
+        steer_angle = -400;
         msg->steer_angle = steer_angle / 10.0;
-        msg->brk = 0.2;
+        msg->brk = 0;
         msg->thr = 0;
-        msg->gear = 0;
-        msg->speed = 0.0;
+        msg->gear = 1;
+        msg->speed = 1.0;
         pub_->publish(*msg);
         ++time_count;
     }
